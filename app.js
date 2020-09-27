626f28dc4f93b3d3b18d20fce2dee06c74511e10
Elizabeth Guerra <ecguerra@outlook.com>
2020-09-27 17:46:20 -0400

    changed to point & click controls

--- app.js
+++ app.js
@@ -27,24 +27,24 @@ function Object(name,x,y,width,height, color) {
 }
 
 furniture = [          
-    lamp = new Object('lamp',650,75,20,400,'#FFFACD'),
+    lamp = new Object('lamp',650,75,30,400,'#FFFACD'),
     pillow = new Object('pillow',500,250,50,50,'#F0F8FF'),
     papers = new Object('papers',10,300,40,100,'white'),
-    rug = new Object('rug',50,500,700,10,'#4682B4'),
-    glass = new Object('glass of water',100,250,20,40,'ghostwhite'),
-    mouse = new Object('toy mouse',500,480,30,10,'hotpink'), 
+    rug = new Object('rug',50,500,700,100,'#4682B4'),
+    coffee = new Object('cup of coffee',100,250,20,40,'peru'),
+    mouse = new Object('toy mouse',500,480,50,30,'hotpink'), 
 ]
 
-const detectHit = thing => {
-    // check for collision on x-axis
-    // if hero's bottom value is > ogre's top value
-    //This part works
-    if (cora.x + cora.width > thing.x &&
-        cora.x < cora.x + thing.width &&
-        cora.y + cora.height > thing.y &&
-        cora.y < cora.y + thing.height
-    ) {
-        thing.notBroken = false
+// const detectHit = thing => {
+//     // check for collision on x-axis
+//     // if hero's bottom value is > ogre's top value
+//     //This part works
+//     if (cora.x + cora.width > thing.x &&
+//         cora.x < cora.x + thing.width &&
+//         cora.y + cora.height > thing.y &&
+//         cora.y < cora.y + thing.height
+//     ) {
+//         thing.notBroken = false
 //-------BUGGY
 //         document.addEventListener('keydown',e =>{
 //             // y = 89 n = 78 // 'KeyY' 'KeyN'
@@ -53,11 +53,32 @@ const detectHit = thing => {
 //             if (e.code==='KeyY') thing.notBroken = false
 //             return
 //         })
-    }
- }
+    // }
+//  }
 
 // const choice = e => {
 // }
+
+const checkCollision = e => {
+    furniture.forEach(thing => {
+        if ( thing.x <= e.offsetX &&
+            thing.x + thing.width >= e.offsetX &&
+            thing.y <= e.offsetY &&
+            thing.y + thing.height >= e.offsetY
+        ){
+            console.log(thing)
+            thing.notBroken = false
+        }
+    })
+}
+
+
+
+const pointAndClick = e => {
+    overlay.classList.add('display-none')
+    cora.x = e.offsetX
+    cora.y = e.offsetY
+}
 
 const gameLoop = () => {
     // console.log('looping 🐱‍🐉')
@@ -67,38 +88,40 @@ const gameLoop = () => {
     // display the x, y coordinates of our hero onto the DOM
     //    movementDisplay.innerText = `x:${cora.x}\ny:${cora.y}`
     // check if the ogre is alive
+    document.addEventListener('click',checkCollision)
     furniture.forEach(thing => {
         if (thing.notBroken) {
             // render the ogre
             thing.render()
             // check for collision
-            furniture.forEach(detectHit)
+    //        furniture.forEach(detectHit)
         }
     })
     // render the hero
+
     cora.render()
     }
 
-const movementHandler = e => {
-    // console.log(e)
-    // w: 87, a: 65, s: 83, d: 68
-    // 'KeyW' 'KeyA' 'KeyS' 'KeyD'
-    overlay.classList.add('display-none')
-    switch(e.code) {
-        case ('KeyW'): // w up
-            if(cora.y > 0) cora.y -= 10
-            break
-        case ('KeyS'): // s down
-            if (cora.y + cora.height < game.height) cora.y += 10
-            break
-        case ('KeyA'): // a left
-            if (cora.x > 0) cora.x -= 10
-            break
-        case('KeyD'): // d right
-            if (cora.x + cora.width < game.width) cora.x += 10
-            break
-    }
-}
+// const movementHandler = e => {
+//     // console.log(e)
+//     // w: 87, a: 65, s: 83, d: 68
+//     // 'KeyW' 'KeyA' 'KeyS' 'KeyD'
+//     overlay.classList.add('display-none')
+//     switch(e.code) {
+//         case ('KeyW'): // w up
+//             if(cora.y > 0) cora.y -= 10
+//             break
+//         case ('KeyS'): // s down
+//             if (cora.y + cora.height < game.height) cora.y += 10
+//             break
+//         case ('KeyA'): // a left
+//             if (cora.x > 0) cora.x -= 10
+//             break
+//         case('KeyD'): // d right
+//             if (cora.x + cora.width < game.width) cora.x += 10
+//             break
+//     }
+// }
 
 // shim layer with setTimeout fallback 
 // consolidates browser variations of RequestAnimationFrame into one function
@@ -129,16 +152,13 @@ document.addEventListener('DOMContentLoaded',()=>{
 
        // context
        ctx = game.getContext('2d')
-    //    ctx.font = '35px Verdana'
-    //    ctx.fillStyle = 'black'
-    //    ctx.fillText = ('Cora Destroys Everything!', 100, 100)
-
     
     //   console.log(furniture)
        
-       cora = new Object('Cora',30,150,20,20, 'black')
+       cora = new Object('Cora',390,450,20,20, 'black')
        
-       document.addEventListener('keydown',movementHandler)
+//     document.addEventListener('keydown',movementHandler)
+        document.addEventListener('click', pointAndClick)
        
     //   let runGame = setInterval(gameLoop, 60) // approx 16-17 fps
         window.requestAnimFrame(gameLoop)    
--- notes.md
+++ notes.md
@@ -9,35 +9,45 @@ Move Cora with WASD and walk up to things in the room to interact with them. You
 ## Objects
 - Lamp
     - Goal: knock over
+    - Level Up: land on pillow stuffing so human doesn't hear
     - Challenge: if knocked over before the pillow is ripped up, the human will hear the crash and come in and catch you
-    - Correct order: TBD
+    - Correct order: 6
 - Pillow
     - Goal: Rip
-    - Challenge: TBD
-    - Correct order: TBD
+    - Challenge: If ripped too early, the wind blows the stuffing around and it doesn't catch the lamp
+    - Correct order: 5
 - Stack of Papers
     - Goal: Knock out window
-    - Challenge: if you wait too long to knock over the stack of paper, a gust of wind blows though the room and knocks it over for you into the room! No credit for the wind's work!
-    - Correct order: TBD
-- Glass of Water
+    - Challenge 1: if you wait too long to knock over the stack of paper, a gust of wind blows though the room and knocks it over into the room!
+    - Challenge 2: if you don't knock the papers over before the coffee, the coffee spills on them
+    - Correct order: 1
+- Cup of Coffee
     - Goal: spill
-    - Challenge: TBD
-        - the human comes in to get it?
-    - Correct order: TBD
+    - Challenge 1: The human comes in to get it if you don't knock it over quickly enough
+    - Challenge 2: It is supposed to be spilled onto the rug, but can also be spilled on the papers
+    - Correct order: 2
 - Rug
-    - Goal: Shred? TBD
-        - The glass spilling affect this
-    - Challenge: TBD
-    - Correct order: TBD
+    - Goal: Bite a hole
+    - Level Up: The coffee spills onto it and leaves a stain
+    - Challenge: If you don't knock the papers over first, the coffee spills on them instead
+    - Correct order: 3
 - Toy Mouse
     - Goal: pull apart
-    - Challenge: Isn't in the room at first. Human brings in when looking for water (?)
-    - Correct order: TBD
+    - Level up 1: If there's a hole in the rug, you put the mouse in it
+    - Challenge: Isn't in the room at first. Human brings in when looking for coffee
+    - Correct order: 4
+
+- Couch - not something to destroy, but you end up sleeping here at the end
+
+## Obstacles
+    - Human comes in to get coffee after turn 2 and leaves the mouse
+    - Gust of wind blows through window after turn 4 and can knock over papers and/or blow stuffing around
     
 ## Wish List
 - Original graphics
     - start with boxes to get game mechanics correct
     - look at sprite packages from game bundle to use in interim
+- Point & Click instead of WASD?
 
 ## Other
 - keyCode is apparently deprecated and you should now use code [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode)
