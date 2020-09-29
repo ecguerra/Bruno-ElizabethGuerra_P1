85f667ddd0665568b66a6379c02d9c721044fd10
Elizabeth Guerra <ecguerra@outlook.com>
2020-09-28 20:12:02 -0400

    added some turn events and updated button function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let game
 let overlay
 let ctx
+let turnCounter
 let lamp
 let pillow
 let papers
@@ -17,6 +18,7 @@ function Object(name,x,y,width,height, color) {
     this.height = height
     this.color = color
     this.notBroken = true
+    this.msg = `The ${this.name} is DESTROYED!`
     this.render = function() {
         ctx.fillStyle = this.color
         ctx.fillRect(this.x,this.y,this.width,this.height)
@@ -27,17 +29,49 @@ function Object(name,x,y,width,height, color) {
 furniture = [          
     lamp = new Object('lamp',650,75,40,400,'#FFFACD'),
     pillow = new Object('pillow',500,250,50,50,'#F0F8FF'),
-    papers = new Object('papers',10,300,40,100,'white'),
+    papers = new Object('stack of papers',10,300,40,100,'white'),
     rug = new Object('rug',50,500,700,100,'#4682B4'),
     coffee = new Object('cup of coffee',100,250,20,40,'peru'),
     mouse = new Object('toy mouse',500,480,60,40,'hotpink'), 
 ]
 
-// When called, creates a new message in the overlay
-const changeMsg = msg => {
+// Object logic
+
+// Each object has a correct order to be broken in and various consequences if you don't
+
+// turnCounter - only go up when an object is broken
+    // Events: 
+        // Turn 2: Human comes in and takes coffee, leaves mouse
+        // Turn 5: Gust of wind through window // affects papers and pillow stuffing
+const counterEvents = () => {
+    if (turnCounter === 2 && coffee.notBroken) {
+        changeMsg (`Your human comes in looking for their coffee. They find it and take it away! Cup of coffee NOT destroyed 💔`,'ok')
+        coffee.notBroken = false
+        turnCounter = 3
+    } else if (turnCounter === 5 && papers.notBroken) {
+        changeMsg(`A gust of wind blows through the open window. The stack of papers falls over! They're destroyed, but you can't take the credit 💔`,'ok')
+        papers.notBroken = false
+        turnCounter = 6
+    } 
+}
+
+
+// 1 - Papers
+    // Destroyed = knocked out window
+    // Can be spilled on by coffee / still playable
+    // Can be blown over by wind / not playable
+
+    papers.spilledOn = false
+    papers.msg = `You push the papers out the window. They are DESTROYED!`
+
+
+
+
+// When called, creates a new message in the overlay // 'type' is yes/no or ok buttons
+const changeMsg = (msg, type) => {
     while (overlay.firstChild) overlay.removeChild(overlay.firstChild)
     overlay.classList.remove('display-none')
-    overlay.style.height = '100px'
+    overlay.style.height = '200px'
     overlay.style.top = '220px'
     let p = document.createElement('p')
     p.innerText = msg
@@ -45,19 +79,27 @@ const changeMsg = msg => {
     let buttonContainer = document.createElement('div')
     buttonContainer.classList.add('button-container')
     overlay.appendChild(buttonContainer)
-    let yesButton = document.createElement('button')
-    yesButton.setAttribute('type','button')
-    yesButton.setAttribute('id','yes-button')
-    yesButton.innerText='yes'
-    buttonContainer.appendChild(yesButton)
-    let noButton = document.createElement('button')
-    noButton.setAttribute('type','button')
-    noButton.setAttribute('id','no-button')
-    noButton.innerText = 'no'
-    buttonContainer.appendChild(noButton)
+    if (type === 'yes') {
+        let yesButton = document.createElement('button')
+        yesButton.setAttribute('type','button')
+        yesButton.setAttribute('id','yes-button')
+        yesButton.innerText='yes'
+        buttonContainer.appendChild(yesButton)
+        let noButton = document.createElement('button')
+        noButton.setAttribute('type','button')
+        noButton.setAttribute('id','no-button')
+        noButton.innerText = 'no'
+        noButton.addEventListener('click', () => overlay.classList.add('display-none'))
+        buttonContainer.appendChild(noButton)
+    } else if (type === 'ok') {
+        let okButton = document.createElement('button')
+        okButton.setAttribute('type','button')
+        okButton.setAttribute('id','ok-button')
+        okButton.innerText='OK'
+        okButton.addEventListener('click', () => overlay.classList.add('display-none'))
+        buttonContainer.appendChild(okButton)
+    }
 }
-
-
 
 // const detectHit = thing => {
 //     // check for collision on x-axis
@@ -80,9 +122,6 @@ const changeMsg = msg => {
     // }
 //  }
 
-// const choice = e => {
-// }
-
 
 // Maybe update with isPointInPath later
 const checkCollision = e => {
@@ -93,9 +132,12 @@ const checkCollision = e => {
                 thing.y <= e.offsetY &&
                 thing.y + thing.height >= e.offsetY
             ){
-                console.log(thing)
-                changeMsg(`Do you want to break the ${thing.name}?`)
-                document.querySelector('#yes-button').addEventListener('click', () => thing.notBroken = false)
+                changeMsg(`Do you want to DESTROY the ${thing.name}?`,'yes')
+                document.querySelector('#yes-button').addEventListener('click', () => {
+                    thing.notBroken = false
+                    turnCounter++
+                    overlay.classList.add('display-none')
+                })
             }
         }
     })
@@ -111,14 +153,14 @@ const pointAndClick = e => {
 //Runs the game, renders all the items
 const gameLoop = () => {
     window.requestAnimFrame(gameLoop)
-    // clear the canvas
     ctx.clearRect(0,0,game.width, game.height)
     document.addEventListener('click',checkCollision)
+    counterEvents()
     furniture.forEach(thing => {
         if (thing.notBroken) {
             thing.render() // when styling, create "broken" sprite for objects
             // check for collision
-    //        furniture.forEach(detectHit)
+            // furniture.forEach(detectHit)
         }
     })
     cora.render()
@@ -173,8 +215,10 @@ document.addEventListener('DOMContentLoaded',()=>{
        // context
        ctx = game.getContext('2d')
 
-       //CHARACTER CONFIG
+       // CHARACTER CONFIG
        cora = new Object('Cora',390,450,20,20, 'black')
+       
+       turnCounter = 0
        
 //     document.addEventListener('keydown',movementHandler)
         document.addEventListener('click', pointAndClick)
